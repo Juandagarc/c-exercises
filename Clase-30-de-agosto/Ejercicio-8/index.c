bc59c8210d36c646243d6f242079dced25bd41a9
Juandagarc <juandagarce@gmail.com>
2023-08-31 22:15:27 -0500

    Ejercicio 8

--- Clase-30-de-agosto/Ejercicio-8/index.c
+++ Clase-30-de-agosto/Ejercicio-8/index.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int inicio, fin, numero;
+    int n, m, conteo = 0, divisor = 1, primo;
+    printf("Ingrese n: ");
+    scanf("%d", &n);
+    printf("Ingrese m: ");
+    scanf("%d", &m);
+    primo = n;
 
-    printf("Ingresa el inicio del rango: ");
-    scanf("%d", &inicio);
-
-    printf("Ingresa el final del rango: ");
-    scanf("%d", &fin);
-
-    printf("Números primos en el rango [%d, %d]: ", inicio, fin);
-
-    if (inicio < 2) {
-        inicio = 2;
+    while (primo < m) {
+        (divisor > primo) ? (
+            (conteo == 2) ? (
+                printf("%d ", primo),
+                conteo = 0,
+                divisor = 1,
+                primo++
+            ) : (
+                conteo = 0,
+                divisor = 1,
+                primo++
+            )
+        ) : (
+            (primo % divisor) == 0 ? (
+                conteo++,
+                divisor++
+            ) : (
+                divisor++
+            )
+        );
     }
-
-    numero = inicio;
-    while (numero <= fin) {
-        int esPrimo = 1;  // Suponemos que el número es primo
-        int divisor = 2;
-
-        while (divisor * divisor <= numero) {
-            if (numero % divisor == 0) {
-                esPrimo = 0;  // Si es divisible por algún número, no es primo
-                break;
-            }
-            divisor++;
-        }
-
-        if (esPrimo) {
-            printf("%d ", numero);
-        }
-
-        numero++;
-    }
-
-    printf("\n");
 
     return 0;
 }
