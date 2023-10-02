a3bc597bfe3d2f27dc3a70ba2c189f68be029f95
Juandagarc <juandagarce@gmail.com>
2023-10-02 10:17:21 -0500

    Taller

--- 2-octubre/invertir-palabra/index.c
+++ 2-octubre/invertir-palabra/index.c
@@ -3,51 +3,50 @@
 
 int main() {
     char sentence[100];
+    char reversedSentence[100];
 
     printf("Ingrese una oración: ");
     fgets(sentence, sizeof(sentence), stdin);
 
     // Elimina el carácter de nueva línea si está presente
     size_t len = strlen(sentence);
-    if (len > 0 && sentence[len - 1] == '\n') {
-        sentence[len - 1] = '\0';
-    }
 
-    char reversedSentence[100];
-    char *wordStart = sentence;
-    char *wordEnd = sentence;
-    char *reversedPtr = reversedSentence;
+    // Agrega un espacio al final del string
+    sentence[len - 1 ] = ' ';  // Agrega el espacio
+    sentence[len] = '\0';  // Añade el carácter nulo para indicar el final del string
+
+
+    int wordStart = 0;
+    int wordEnd = 0;
+    int reversedIndex = 0;
 
     // Recorre la cadena
-    while (*wordEnd) {
-        wordEnd++;
-
+    while (sentence[wordEnd] || (wordEnd == 0 && len > 0)) {
         // Si se encuentra un espacio o el final de la cadena, procesa la palabra
-        if (*wordEnd == ' ' || *wordEnd == '\0') {
+        if (sentence[wordEnd] == ' ' || sentence[wordEnd] == '\0') {
             // Copia la palabra invertida en la cadena invertida
-            char *start = wordStart;
-            while (start < wordEnd) {
-                *reversedPtr = *(wordEnd - 1);
-                reversedPtr++;
-                wordEnd--;
+            for (int i = wordEnd - 1; i >= wordStart; i--) {
+                reversedSentence[reversedIndex++] = sentence[i];
             }
 
-            // Agrega un espacio si no es el final de la cadena
-            if (*wordEnd == ' ') {
-                *reversedPtr = ' ';
-                reversedPtr++;
+            // Verifica si es la última palabra antes de agregar un espacio
+            if (sentence[wordEnd] == ' ' && sentence[wordEnd + 1] != '\0') {
+                reversedSentence[reversedIndex++] = ' ';
             }
 
-            // Actualiza el puntero de inicio de la siguiente palabra
-            wordStart = wordEnd ;
+            // Actualiza el índice de inicio de la siguiente palabra
+            wordStart = wordEnd + 1;
         }
+
+        // Incrementa wordEnd solo si no hemos llegado al final de la cadena
+        wordEnd++;
     }
 
     // Agrega el carácter nulo al final de la cadena invertida
-    *reversedPtr = '\0';
+    reversedSentence[reversedIndex] = '\0';
 
-    // Imprime la oración invertida
-    printf("Oración invertida: %s\n", reversedSentence);
+    // Imprime la oración invertida, sin salto de línea para la última palabra
+    printf("Oración invertida: %s", reversedSentence);
 
     return 0;
 }
