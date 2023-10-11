d0246d81a41fa4d4c8841ab3d96292d9c7450f79
Juandagarc <juandagarce@gmail.com>
2023-10-11 09:00:11 -0500

    Ejercicio

--- 11-octubre/index.c
+++ 11-octubre/index.c
@@ -1,116 +1,116 @@
-//Presentado por:Juan David García Arce
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
-//se llena con random
-void llenarMatriz(int matriz[3][3]){
-    int i,j;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            matriz[i][j]=rand()%10 + 1;
+
+void llenarMatriz(int matriz[100][100], int n, int m) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            matriz[i][j] = rand() % 10 + 1;
         }
     }
 }
-void mostrarMatriz(int matriz[3][3]){
-    int i,j;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            printf("%d\t",matriz[i][j]);
+
+void mostrarMatriz(int matriz[100][100], int n, int m) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("%d\t", matriz[i][j]);
         }
         printf("\n");
     }
 }
-int esPrimo(int numero){
-    int i,contador=0;
-    for (i=1;i<=numero;i++){
-        if (numero%i==0){
+
+int esPrimo(int numero) {
+    int i, contador = 0;
+    for (i = 1; i <= numero; i++) {
+        if (numero % i == 0) {
             contador++;
         }
     }
-    if (contador==2){
-        return 1;
-    }else{
-        return 0;
-    }
+    return contador == 2;
 }
-int sumaDiagonalPrimos(int matriz[3][3]){
-    int i,j,suma=0;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            if (i==j){
-                if (esPrimo(matriz[i][j])==1){
-                    suma+=matriz[i][j];
-                }
-            }
+
+int sumaDiagonalPrimos(int matriz[100][100], int n, int m) {
+    int i, j, suma = 0;
+    int min_dim = n < m ? n : m; // Tomar la dimensión más pequeña para la diagonal principal
+
+    for (i = 0; i < min_dim; i++) {
+        if (esPrimo(matriz[i][i])) {
+            suma += matriz[i][i];
         }
     }
     return suma;
 }
-void llenarVectoresParesImpares(int matriz[3][3], int pares[], int impares[]){
-    //Busca pares e impares
-    int i,j,k=0,l=0;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            if (matriz[i][j]%2==0){
-                pares[k]=matriz[i][j];
+
+void llenarVectoresParesImpares(int matriz[100][100], int pares[], int impares[], int n, int m) {
+    int i, j, k = 0, l = 0;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            if (matriz[i][j] % 2 == 0) {
+                pares[k] = matriz[i][j];
                 k++;
-            }else{
-                impares[l]=matriz[i][j];
+            } else {
+                impares[l] = matriz[i][j];
                 l++;
             }
         }
     }
 }
-void mostrarVectores(int pares[], int impares[]){
+
+void mostrarVectores(int pares[], int impares[], int n, int m) {
     int i;
-    //Se muestra hhasta encontrar numeros menores a 10
     printf("Pares: ");
-    for (i=0;i<9;i++){
-        if (pares[i]<10 && pares[i]>0){
-            printf("%d\t",pares[i]);
+    for (i = 0; i < n * m; i++) {
+        if (pares[i] < 10 && pares[i] > 0) {
+            printf("%d\t", pares[i]);
         }
     }
 
     printf("\nImpares: ");
-    for (i=0;i<9;i++){
-        if (impares[i]<10 && impares[i]>0){
-            printf("%d\t",impares[i]);
+    for (i = 0; i < n * m; i++) {
+        if (impares[i] < 10 && impares[i] > 0) {
+            printf("%d\t", impares[i]);
         }
     }
     printf("\n");
 }
-void elevarAlCuadrado(int matriz[3][3]){
-    int i,j;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            matriz[i][j]=matriz[i][j]*matriz[i][j];
+
+void elevarAlCuadrado(int matriz[100][100], int n, int m) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            matriz[i][j] = matriz[i][j] * matriz[i][j];
         }
     }
 }
-void sumarFilasPares(int matriz[3][3]){
-    int i,j,suma=0;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            if (matriz[i][j]%2==0){
-                suma+=matriz[i][j];
+
+void sumarFilasPares(int matriz[100][100], int n, int m) {
+    int i, j, suma = 0;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            if (matriz[i][j] % 2 == 0) {
+                suma += matriz[i][j];
             }
         }
-        printf("La suma de la fila %d es: %d\n",i+1,suma);
-        suma=0;
+        printf("La suma de la fila %d es: %d\n", i + 1, suma);
+        suma = 0;
     }
 }
-void ordenarDiagonalSecundaria(int matriz[3][3]){
-    int i,j,k,aux;
-    for (i=0;i<3;i++){
-        for (j=0;j<3;j++){
-            if (i+j==2){
-                for (k=0;k<3;k++){
-                    if (matriz[i][j]<matriz[k][k]){
-                        aux=matriz[i][j];
-                        matriz[i][j]=matriz[k][k];
-                        matriz[k][k]=aux;
+
+void ordenarDiagonalSecundaria(int matriz[100][100], int n, int m) {
+    int i, j, k, aux;
+    int min_dim = n < m ? n : m; // Tomar la dimensión más pequeña para la diagonal secundaria
+
+    for (i = 0; i < min_dim; i++) {
+        for (j = 0; j < min_dim; j++) {
+            if (i + j == min_dim - 1) {
+                for (k = 0; k < min_dim; k++) {
+                    if (matriz[i][j] < matriz[k][k]) {
+                        aux = matriz[i][j];
+                        matriz[i][j] = matriz[k][k];
+                        matriz[k][k] = aux;
                     }
                 }
             }
@@ -120,10 +120,11 @@ void ordenarDiagonalSecundaria(int matriz[3][3]){
 
 int main() {
     srand(time(NULL));
-    int matriz[3][3];
-    int pares[9];
-    int impares[9];
+    int matriz[100][100];
+    int pares[100 * 100];
+    int impares[100 * 100];
     int opcion;
+    int n, m;
 
     do {
         printf("\nMenu\n");
@@ -140,26 +141,31 @@ int main() {
 
         switch (opcion) {
             case 1:
-                llenarMatriz(matriz);
+                printf("Ingrese el tamaño de la matriz\n");
+                printf("Filas: ");
+                scanf("%d", &n);
+                printf("Columnas: ");
+                scanf("%d", &m);
+                llenarMatriz(matriz, n, m);
                 break;
             case 2:
-                mostrarMatriz(matriz);
+                mostrarMatriz(matriz, n, m);
                 break;
             case 3:
-                printf("La suma de primos en la diagonal principal es: %d\n", sumaDiagonalPrimos(matriz));
+                printf("La suma de primos en la diagonal principal es: %d\n", sumaDiagonalPrimos(matriz, n, m));
                 break;
             case 4:
-                llenarVectoresParesImpares(matriz, pares, impares);
-                mostrarVectores(pares, impares);
+                llenarVectoresParesImpares(matriz, pares, impares, n, m);
+                mostrarVectores(pares, impares, n, m);
                 break;
             case 5:
-                elevarAlCuadrado(matriz);
+                elevarAlCuadrado(matriz, n, m);
                 break;
             case 6:
-                sumarFilasPares(matriz);
+                sumarFilasPares(matriz, n, m);
                 break;
             case 7:
-                ordenarDiagonalSecundaria(matriz);
+                ordenarDiagonalSecundaria(matriz, n, m);
                 break;
             case 8:
                 printf("Saliendo del programa.\n");
